2f84724b1b69499e075266b93a5c8633cf3638ad
Захаров Павел <pzakharov83@gmail.com>
2023-02-01 16:57:06 +0300

    new translate

--- kiosk_upload/kiosk_front/admin.py
+++ kiosk_upload/kiosk_front/admin.py
@@ -1,11 +1,18 @@
 """Admin forms for kiosks."""
 
 from django.contrib import admin
-from kiosk_front.models import Kiosk
+from kiosk_front.models import Cinema, Kiosk
 
 
 @admin.register(Kiosk)
 class KioskAdmin(admin.ModelAdmin):
     """Kiosk Admin model."""
+
+    pass
+
+
+@admin.register(Cinema)
+class CinemaAdmin(admin.ModelAdmin):
+    """Cinema Admin model."""
 
     pass
--- kiosk_upload/kiosk_upload/components/_locale.py
+++ kiosk_upload/kiosk_upload/components/_locale.py
@@ -1,7 +1,7 @@
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'ru-ru'
+LANGUAGE_CODE = 'ru'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
